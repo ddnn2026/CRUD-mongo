8833b363f6d7dfcc81a0ee6025165c91861a0caf
david nguyen <ddnn2026>
2023-04-21 00:38:07 +0700

    update

--- README.md
+++
@@ -1,8 +0,0 @@
-# Database
-
-* docker compose up
-* mongose express 8081
-
-# app server
-
-* npm start
--- services/userService.js
+++
@@ -1,9 +0,0 @@
-const userModel = require("../models/User");
-
-exports.createUser = async (element) => {
-  return await userModel.create(element);
-};
-
-exports.findNameUser = async (element)=>{
-  return await userModel.findOne(element)
-}
